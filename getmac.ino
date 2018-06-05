bf015d0069a1790a424947cf5d9979aab37bb1ba
Martin <martinius96@gmail.com>
2018-06-05 02:00:08 +0200

    Update getmac.ino

--- getmac.ino
+++ getmac.ino
@@ -1,13 +1,30 @@
-//sketch na zistenie MAC adresy dosky ESP32
+//sketch na zistenie MAC adresy dosky ESP32, ktorú je nutné zadať na userpanel
 #include <WiFi.h>
 
-void setup() {
+byte mac[6];
+
+void setup()
+{
     Serial.begin(115200);
-    delay(10);
-    Serial.println();
+    delay(1000);
+    WiFi.begin();
+    Serial.println("WiFi spustene");
+    WiFi.macAddress(mac);   
+    Serial.print("MAC adresa: ");
+    Serial.print(mac[0],HEX);
+    Serial.print(":");
+    Serial.print(mac[1],HEX);
+    Serial.print(":");
+    Serial.print(mac[2],HEX);
+    Serial.print(":");
+    Serial.print(mac[3],HEX);
+    Serial.print(":");
+    Serial.print(mac[4],HEX);
+    Serial.print(":");
+    Serial.println(mac[5],HEX);
+
 }
 
-void loop() {
- Serial.println("MAC adresa: ");
-   Serial.println(WiFi.macAddress());
+void loop(){
+
 }
